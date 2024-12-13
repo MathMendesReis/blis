57aa4bd94e8ecb2d74c5e85992311b26f03d265c
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-13 17:41:03 -0300

    feat: implement basic Express server setup in app.ts and server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import { app } from "./app"
+import { env } from "./utils/_env"
+
+const PORT = env.data?.PORT
+
+app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
