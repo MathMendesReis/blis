09b41b533b9cf9a84d7a31e8acad2a771b464168
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-15 08:52:02 -0300

    feat: adicionar testes de integração para o controlador de sessão

---
+++ src/controllers/session-controller.test.ts
@@ -0,0 +1,36 @@
+import request from 'supertest'
+import { app } from '../app'
+import { prisma } from '../database/prisma'
+describe('Session controller (e2)',()=>{
+  beforeEach(async () => {
+    await prisma.$connect()
+    await prisma.user.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await prisma.user.deleteMany({
+      where:{
+        email: 'any@example.com',
+      }
+    })
+    await prisma.$disconnect(); 
+  });
+    it('should return 201 if user is login',async()=>{
+        const createUser = await request(app).post('/api/users').send({
+        name: 'any_name',
+        birthdate: '2021-09-09',
+        email: 'any@example.com',
+        password: '123456',
+      })
+     
+      const response = await request(app).post('/api/session/login').send({
+        name: 'any_name',
+        birthdate: '2021-09-09',
+        email: 'any@example.com',
+        password: '123456',
+      })
+      expect(response.status).toBe(200)
+      expect(response.body).toHaveProperty('token')
+    })
+    
+})
