9debf80232496c9de7a1bc3f91cd0bcea82d1f69
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-15 08:52:48 -0300

    feat: adicionar validação de variáveis de ambiente e novos campos para configuração

--- src/utils/_env.ts
+++ src/utils/_env.ts
@@ -2,8 +2,16 @@ import z, { coerce } from "zod";
 
 const envSchema = z.object({
   PORT:z.coerce.number().default(3000),
+  DATABASE_URL:z.string(),
+  JWT_SECRET:z.string(),
+  JWT_EXPIRES_IN:z.string(),
 })
 
-const env = envSchema.safeParse(process.env)
+const envSchemaValidation = envSchema.safeParse(process.env)
+if (!envSchemaValidation.success) {
+  console.error(envSchemaValidation.error.issues);
+  throw new Error('There is an error with the server environment variables');
+}
 
+ const env = envSchemaValidation.data;
 export {env}
