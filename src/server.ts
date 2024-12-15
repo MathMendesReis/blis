8ed59751e57731494827de74ec288cdae8eede0c
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-15 08:51:54 -0300

    feat: adicionar configuração de autenticação JWT e corrigir acesso à variável de ambiente PORT

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from "./app"
 import { env } from "./utils/_env"
 
-const PORT = env.data?.PORT
+const PORT = env.PORT
 
 app.listen(PORT,()=>console.log(`Server is running on port ${PORT}`))
