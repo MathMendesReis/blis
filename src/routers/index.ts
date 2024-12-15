8899f3b7f13a154f73b5db10c8de086e0f3bd004
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-15 08:52:41 -0300

    feat: adicionar roteador de sessão para gerenciamento de autenticação

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { userRouter } from "./user-routers";
+import { sessionRouter } from "./session-router";
 
 const router = Router()
 router.use('/api/users', userRouter)
+router.use('/api/session', sessionRouter)
 export { router }
