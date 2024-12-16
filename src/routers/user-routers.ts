9fc743db080091d3e0807c09d1706539ab86ad0b
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-16 15:28:27 -0300

    feat: adicionar roteadores para gerenciamento de habilidades de usuário

--- src/routers/user-routers.ts
+++ src/routers/user-routers.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/create-user-controller";
-import { UserDocumentController } from "../controllers/users-document-controller";
-import { upload } from "../utils/multer-config";
 import { ensureAuthenticate } from "../midlewares/ensure-authenticate";
+import { CreateUserAbilitiesController } from "../controllers/create-user-abilities-controller";
+import { DeleteAbilitiesController } from "../controllers/delete-abilities-controller";
+import { ListUserAbilitiesController } from "../controllers/list-user-abilities-controller";
 
 const userRouter = Router();
 userRouter.post('/',new CreateUserController().handle );
-userRouter.post('/documents',ensureAuthenticate, upload.single('uploaded_file'),new UserDocumentController().handle );
+userRouter.post('/abilities',ensureAuthenticate, new CreateUserAbilitiesController().handle );
+userRouter.delete('/abilities',ensureAuthenticate, new DeleteAbilitiesController().handle );
+userRouter.get('/abilities',ensureAuthenticate, new ListUserAbilitiesController().handle );
 export { userRouter };
