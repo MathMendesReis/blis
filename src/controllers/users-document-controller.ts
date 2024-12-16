2ba9f309a2cbaa6aa6f17258eb3a056ce15f39bf
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-15 21:14:27 -0300

    feat: adicionar validação de entrada e persistência de documentos de usuário

--- src/controllers/users-document-controller.ts
+++ src/controllers/users-document-controller.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
+import z from 'zod';
+import { prisma } from '../database/prisma';
 export class UserDocumentController {
   async handle(req:Request, res:Response) {
+  
+    const bodySchema = z.object({
+      name:  z.string(),
+    })
+    const {name} =bodySchema.parse(req.body)
+
+    if (req.file) {
+      const fileUrl = `http://localhost:3000/uploads/${req.file.filename}`;
+      await prisma.userDocuments.create({
+        data:{
+          name: name,         
+          url: fileUrl,        
+          userId:req.user?.id as string
+
+        }
+      })
+    }
+
     res.status(201).json({message:"User document created"})
   }
 }
