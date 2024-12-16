fad1e40c50b908abaef706eb38e7a872039fe32e
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-16 15:27:45 -0300

    feat: adicionar controlador para listar habilidades de usuário

---
+++ src/controllers/list-user-abilities-controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import z from "zod";
+import { prisma } from "../database/prisma";
+export class ListUserAbilitiesController {
+  async handle(req:Request, res:Response) {
+  
+    const response = await prisma.user.findMany({
+      where:{
+        id:req.user?.id as string
+      },
+      select:{
+        id:true,
+        name:true,
+        email:true,
+        birthdate:true,
+        UsersAbilities:{
+          select:{
+            yearsExperience:true,
+            ability:{
+              select:{
+                name:true
+              }
+            }
+          },
+          orderBy:{
+            created_at:"desc"
+          }
+        }
+      }
+    })
+
+
+    res.status(200).json({response})
+  } 
+}
+
