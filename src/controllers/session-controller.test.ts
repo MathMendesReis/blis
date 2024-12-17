8e43409b321e7e3ed8d4d3a4b6db7a37b8d3fe68
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-17 08:18:21 -0300

    test: atualizar testes de integração para o controlador de sessão

--- src/controllers/session-controller.test.ts
+++ src/controllers/session-controller.test.ts
@@ -3,22 +3,19 @@ import { app } from '../app'
 import { prisma } from '../database/prisma'
 describe('Session controller (e2)',()=>{
   beforeEach(async () => {
-    await prisma.$connect()
-    await prisma.user.deleteMany({})
+    await request(app).post('/api/users').send({
+      name: 'any_name',
+      birthdate: '2021-09-09',
+      email: 'any@example.com',
+      password: '123456',
+    })
   })
-
+  
   afterAll(async () => {
     await prisma.user.deleteMany()
     await prisma.$disconnect(); 
   });
     it('should return 201 if user is login',async()=>{
-        const createUser = await request(app).post('/api/users').send({
-        name: 'any_name',
-        birthdate: '2021-09-09',
-        email: 'any@example.com',
-        password: '123456',
-      })
-     
       const response = await request(app).post('/api/session/login').send({
         name: 'any_name',
         birthdate: '2021-09-09',
@@ -27,6 +24,15 @@ describe('Session controller (e2)',()=>{
       })
       expect(response.status).toBe(200)
       expect(response.body).toHaveProperty('token')
+    })
+    it('should return 401',async()=>{
+      const response = await request(app).post('/api/session/login').send({
+        name: 'any_name',
+        birthdate: '2021-09-09',
+        email: 'any@example.com',
+        password: '123458',
+      })
+      expect(response.status).toBe(401)
     })
     
 })
