8e1d8fb5bdf8096910fbde1270c855b66431e02b
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-17 08:18:35 -0300

    fix: corrigir tipo de erro no filtro de arquivos para rejeição de não-imagens

--- src/utils/file-filter.ts
+++ src/utils/file-filter.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express';
 import multer, { FileFilterCallback } from 'multer';
 
-const fileFilter = (req: Request, file:any, cb: FileFilterCallback) => {
+const fileFilter = (req: Request, file: any, cb: FileFilterCallback) => {
   // Aceitar apenas arquivos com as extensões .jpg, .jpeg, .png, .gif
   if (file.mimetype.startsWith('image/')) {
     cb(null, true);  // Aceita o arquivo
   } else {
-    cb(new Error('Somente imagens são permitidas'), false);  // Rejeita o arquivo
+    cb(new Error('Somente imagens são permitidas') as unknown as null, false);  // Rejeita o arquivo
   }
 };
 
