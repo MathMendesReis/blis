1330c4a836b1faa49043ac874a04a5511d2d2f0e
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-23 11:41:12 -0300

    feat: substituir controlador de exclusão de habilidades por uma fábrica para melhor abstração

--- src/routers/user-routers.ts
+++ src/routers/user-routers.ts
@@ -5,10 +5,11 @@ import { CreateUserAbilitiesController } from "../controllers/create-user-abilit
 import { DeleteAbilitiesController } from "../controllers/delete-abilities-controller";
 import { ListUserAbilitiesController } from "../controllers/list-user-abilities-controller";
 import { makeCreateUserController } from "../factories/make-create-user-controller";
+import { makeDeleteAbilitiesController } from "../factories/make-delete-abilitie-controller";
 
 const userRouter = Router();
 userRouter.get('/abilities',ensureAuthenticate, new ListUserAbilitiesController().handle );
 userRouter.post('/',makeCreateUserController().handle );
 userRouter.post('/abilities',ensureAuthenticate, new CreateUserAbilitiesController().handle );
-userRouter.delete('/abilities',ensureAuthenticate, new DeleteAbilitiesController().handle );
+userRouter.delete('/abilities',ensureAuthenticate, makeDeleteAbilitiesController().handle );
 export { userRouter };
