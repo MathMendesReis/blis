be4cc04b261d3235238bba1c565f73a0bfff0315
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-23 10:46:00 -0300

    feat: substituir controlador de criação de usuário por uma fábrica para melhor abstração

--- src/routers/user-routers.ts
+++ src/routers/user-routers.ts
@@ -4,10 +4,11 @@ import { ensureAuthenticate } from "../midlewares/ensure-authenticate";
 import { CreateUserAbilitiesController } from "../controllers/create-user-abilities-controller";
 import { DeleteAbilitiesController } from "../controllers/delete-abilities-controller";
 import { ListUserAbilitiesController } from "../controllers/list-user-abilities-controller";
+import { makeCreateUserController } from "../factories/make-create-user-controller";
 
 const userRouter = Router();
 userRouter.get('/abilities',ensureAuthenticate, new ListUserAbilitiesController().handle );
-userRouter.post('/',new CreateUserController().handle );
+userRouter.post('/',makeCreateUserController().handle );
 userRouter.post('/abilities',ensureAuthenticate, new CreateUserAbilitiesController().handle );
 userRouter.delete('/abilities',ensureAuthenticate, new DeleteAbilitiesController().handle );
 export { userRouter };
