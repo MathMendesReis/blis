654574f045422c133943c88363b6702a73673d28
Matheus Mendes <matheusmendesreis@outlook.com>
2024-12-23 11:41:33 -0300

    feat: refatorar controlador de exclusão de habilidades para utilizar caso de uso

--- src/controllers/delete-abilities-controller.ts
+++ src/controllers/delete-abilities-controller.ts
@@ -1,27 +1,18 @@
 import { Request, Response } from "express";
 import z from "zod";
-import { prisma } from "../database/prisma";
-import { AppError } from "../utils/app-error";
+import type { DeleteAbilitiesUseCase } from "../domain/users/use-case/delete-abilitie-use-case";
 export class DeleteAbilitiesController {
-  async handle(req:Request, res:Response) {
-  
+  constructor(
+    private readonly deleteAbilitiesUseCase: DeleteAbilitiesUseCase
+  ){}
+
+  public handle = async (req:Request, res:Response) => {
     const bodySchema = z.object({
       abilitiesId:z.array(z.string()),
     })
     const {abilitiesId} =bodySchema.parse(req.body)
-
-    await prisma.usersAbilities.deleteMany({
-      where:{
-        userId:req.user?.id as string,
-        abilityId:{
-          in:abilitiesId
-        }
-      }
-    })
-  
-
-
+    await this.deleteAbilitiesUseCase.execute({userId:req.user?.id as string,abilitiesId})
     res.status(204).json()
-  } 
+  }
 }
 
